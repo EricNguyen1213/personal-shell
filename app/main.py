f7747d3b641d724496d16212ced8565952a2adf0
EricNguyen1213 <eric.nguyen1213@gmail.com>
2026-02-17 19:30:50 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,35 @@
 import sys
 
 
+def not_found(cmd_line):
+    return lambda _: print(f"{cmd_line}: command not found")
+
+
+def handle_exit(_):
+    return sys.exit(0)
+
+
+def handle_echo(args):
+    return print(" ".join(args))
+
+
+command_lib = {"exit": handle_exit, "echo": handle_echo}
+
+
 def main():
+
     while True:
         sys.stdout.write("$ ")
         user_input = input()
-        if user_input == "exit":
-            return
-        print(f"{user_input}: command not found")
+        cmd_line = user_input.split()
+
+        if not cmd_line:
+            continue
+
+        cmd, *args = cmd_line
+
+        command_func = command_lib.get(cmd, not_found(user_input))
+        command_func(args)
 
     pass
 
