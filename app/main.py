ca5b1ffb0e41842c5852ec23caad5b27f8e5fd7b
EricNguyen1213 <eric.nguyen1213@gmail.com>
2026-02-18 04:27:45 -0500

    Implement Custom Exec Commands)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
 import sys
 import os
 from pathlib import Path
+import subprocess
 
 
-def not_found(cmd_line):
-    return lambda _: print(f"{cmd_line}: command not found")
+def find_which_path(fn):
+    # Construct And Check Validity of Path to Possible File Input
+    paths = os.getenv("PATH", "").split(os.pathsep)
+    valid_file_path = next(
+        (
+            file_path
+            for file_path in (Path(f"{path}/{fn}") for path in paths)
+            if file_path.exists()
+            and file_path.is_file()
+            and os.access(file_path, os.X_OK)
+        ),
+        None,
+    )
+    return valid_file_path
+
+
+def not_found(user_input):
+    return lambda _: print(f"{user_input}: command not found")
 
 
 # exit command case
@@ -25,26 +42,18 @@ def handle_type(args):
             print(f"{arg} is a shell builtin")
             continue
 
-        # Construct Path to Possible File Input
-        file_paths = [
-            Path(f"{path}/{arg}") for path in os.getenv("PATH").split(os.pathsep)
-        ]
-
-        # Checks if File Exists or Is Executable
-        found_file_path = next(
-            (
-                file_path
-                for file_path in file_paths
-                if file_path.exists()
-                and file_path.is_file()
-                and os.access(file_path, os.X_OK)
-            ),
-            None,
-        )
+        found_file_path = find_which_path(arg)
         if found_file_path:
             print(f"{arg} is {found_file_path}")
         else:
             print(f"{arg} not found")
+
+
+def handle_custom_exec(cmd_line, user_input):
+    file_path = find_which_path(cmd_line[0])
+    if file_path:
+        return lambda _: subprocess.run(cmd_line)
+    return not_found(user_input)
 
 
 command_lib = {"exit": handle_exit, "echo": handle_echo, "type": handle_type}
@@ -63,9 +72,13 @@ def main():
         cmd, *args = cmd_line
 
         # Search Command Library for Correct Function To Use
-        command_func = command_lib.get(cmd, not_found(user_input))
-        command_func(args)
+        command_func = command_lib.get(cmd, None)
 
+        # Non Built In Case
+        if not command_func:
+            command_func = handle_custom_exec(cmd_line, user_input)
+
+        command_func(args)
     pass
 
 
