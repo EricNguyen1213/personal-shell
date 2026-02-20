55912271f934226e49fe4fc861a847b54e79bb6c
EricNguyen1213 <eric.nguyen1213@gmail.com>
2026-02-19 20:06:18 -0500

    Implement Backslash Escaped In Single Quotes

--- app/utils.py
+++ app/utils.py
@@ -2,18 +2,23 @@ import re
 
 
 OUTSIDE_WHITESPACE = re.compile(r"""(?<!\\)\s+(?=(?:[^'"]|'[^']*'|"[^"]*")*$)""")
-TOP_QUOTE_UNWRAPPER = re.compile(r"""((?<!\\)(\'|"|'))(.*?)\1""")
-ESCAPE_BACKSLASH = re.compile(r"""\\(?=\\)|(?<!\\)\\""")
+TOP_QUOTE_UNWRAPPER = r"""(?s)(((?<!\\)(\'|"|'))(.*?)\3)"""
+ESCAPE_BACKSLASH = r"""\\(?=\\)|(?<!\\)\\"""
+COMBINED_PATTERN = re.compile(f"{TOP_QUOTE_UNWRAPPER}|{ESCAPE_BACKSLASH}")
+
+
+def resolve_escapes_and_quotes(m):
+    # If Top-Most Quotes (Avoiding Escaped Quotes Case) Found -> Return Its Body Unchanged
+    if m.group(1):
+        return m.group(4)
+
+    # Remove Backslashes signifying Escapedness
+    return ""
 
 
 def sanitize(user_input):
     # Split by Whitespace Entirely Outside of Quotes (Avoiding Escaped Cases)
     cmd, *args = re.split(OUTSIDE_WHITESPACE, user_input)
 
-    # Remove Top-Most Quotes (Avoiding Escaped Cases):  "'hello'"'"world"'  -> 'hello'"world"
-    args = [re.sub(TOP_QUOTE_UNWRAPPER, r"\3", arg) for arg in args]
-
-    # Remove Escape Backslashes
-    args = [re.sub(ESCAPE_BACKSLASH, "", arg) for arg in args]
-
+    args = [re.sub(COMBINED_PATTERN, resolve_escapes_and_quotes, arg) for arg in args]
     return cmd, args
